9a986256df5041d6038498e065ef75e4e0414d02
Mahtab Syed <mahtab.syed@gmail.com>
2022-08-10 15:19:10 +1000

    Added Notification context and footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
+import { NotificationContextProvider } from "../store/notification-context";
 import "../styles/globals.css";
 
 // _app is the root component
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
